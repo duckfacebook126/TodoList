80f479b28f990e0496b74fb39c74963c7e93a2b1
duckfacebook126 <duckfaceb126@gmail.com>
2025-04-07 17:03:25 +0500

    Side completed and made responsive

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect,useState, useRef } from 'react'
 import { LogOut } from 'lucide-react';
 import { Menu } from 'lucide-react';
 import { CircleUserRound } from 'lucide-react';
@@ -8,14 +8,55 @@ import Sidebar from './sidebar';
 const Navbar = () => {
 
 const {logout}=authStore();
-
+//show the Side bar
 const [showSideBar, setShowSideBar]=useState(false);
-const sidebarRef=useRef()
+
+const[sidebarClickMenu,setSidebarClickMenu]=useState(1);
+
+const handleMenuClickCounter = () => {
+  if (sidebarClickMenu === 1) {
+    setSidebarClickMenu(0); // Corrected: Call the function with the new value
+  } else if (sidebarClickMenu === 0) {
+    setSidebarClickMenu(1); // Corrected: Call the function with the new value
+  }
+};
+
+
+const handleMenuClick=()=>
+{
+  handleMenuClickCounter();
+  toggleVisibility();
+}
+
+
+//set use ref for the side bar visibility
+const sidebarRef=useRef(null);
+
+useEffect(() => {
+
+    
+}, []);
+
+
+
+const toggleVisibility=()=>{
+setShowSideBar(!showSideBar);
+}
+
   return (
+    <>
+  
+  {/* Sidebar Component TO be shown or hidden */}
+<div ref={sidebarRef} className={`fixed top-[88px] left-0 h-full w-64 bg-base shadow-md z-50 transistion-transform duration-300 ${showSideBar&&sidebarClickMenu===0? 'transalate-x-0': '-translate-x-full'}`}>
+<Sidebar>
+
+</Sidebar>
+</div>
+
 <div className="navbar bg-secondary shadow-sm">
   <div className="flex justify-between items-center w-full p-4">
     {/* Side bar Button On the Left */}
-    <button className="ml-0 btn btn-base" onClick={()=>setShowSideBar(true)}>
+    <button className="ml-0 btn btn-base" onClick={handleMenuClick}>
       <Menu />
     </button>
   
@@ -36,7 +77,8 @@ const sidebarRef=useRef()
 
   </div>
 </div>
+</>
   )
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { AlarmClockCheck } from 'lucide-react';//for to lsit
+import { Loader } from 'lucide-react'; //for INprogress Tasks
+import { CheckCheck } from 'lucide-react';//for completted tasks
 
 const Sidebar = () => {
+
+
+const SidebarItems=[
+  'ToDo',
+  'In Progress',
+  'Done',
+  'Progress Report'
+];
+
   return (
-    <div> Sidebar
-        <div>
-            
-        </div>
+    <div className="fixed bg-base left-0 h-full w-64 shadow-2xl items-center justify-center">
+      <div className="flex flex-col ">
+        {SidebarItems.map((item, index) => (
+          <button
+            key={index}
+
+            className={'p-4 w-full text-left hover:bg-base-300'
+            }
+          >
+
+
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
-  )
+  ) 
 }
 
 export default Sidebar
